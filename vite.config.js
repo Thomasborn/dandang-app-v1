fd9871fff98dd2997297e4da9f7e5b3119e07ed2
Thomas <thomasbtbx@gmail.com>
2023-12-09 12:02:58 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dandang-app-v1/',
   plugins: [vue()],
-  base: "/dandang-app-v1/",
-  server: {
-    port: '3000',
-  },
-  preview: {
-    port: '8080'
-  }
-})
+});
